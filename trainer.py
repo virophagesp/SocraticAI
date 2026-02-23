2db028dd5db2152ca0e8f998c0511b37a97c9ffd
virophagesp <virophagesp4444@gmail.com>
2026-02-23 14:38:38 +1300

    merged preprocessor.py into trainer.py

--- trainer.py
+++ trainer.py
@@ -32,9 +32,42 @@ DROPOUT = 0.2
 # The size of self-attention heads
 HEAD_SIZE = N_EMBD // N_HEAD
 
+
+# process the raw json data and convert it to usable text
+def raw_to_processed(raw_data):
+    output_data = ""
+    # Loop over data and write to input file
+    data_looper = 0
+    # only half the length since it will be using two items at a time
+    while data_looper < int(len(raw_data)/2):
+        # Get the question, every even number are the strings
+        question = raw_data[data_looper*2]
+        # Loop over the answers, every odd number are the lists
+        for answer in raw_data[data_looper*2+1]:
+            # Add the same amount of newline characters
+            #  as block_size in the training script
+            # The reason why this is done is because
+            #  the ai will incorrectly learn to try
+            #  to predict more questions after answering
+            #  this not only is not something it should learn
+            #  but could cause issues and confusion for users
+            #  if it seemingly starts getting new weird inputs
+            #  that weren't typed in
+            for i in range(128):
+                output_data += '\n'
+            # Print question then answer
+            output_data += f'question: "{question}"\n'
+            output_data += f'answer: "{answer}"\n'
+        data_looper += 1
+    return output_data
+
+
+# Open and read the raw training and testing data from the JSON files
+raw_training_data = json.load(open('questions.json', 'r'))
+raw_testing_data = json.load(open('testing.json', 'r'))
 # Open the files containing output of using preprocessor
-text = open('input.txt', 'r', encoding='utf-8').read().lower()
-validation = open('validation.txt', 'r', encoding='utf-8').read().lower()
+text = raw_to_processed(raw_training_data).lower()
+validation = raw_to_processed(raw_testing_data).lower()
 
 # file that records testing
 testing_file = open('testing.txt', 'w')
